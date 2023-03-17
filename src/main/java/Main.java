3c3f6ee67c76b4cbcf33046b6dda6d9c3d49749a
muhammadreza abdollahnejad <reza123.reza987@gmail.com>
2023-03-17 16:39:41 +0330

    impl update method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,21 +4,26 @@ import repository.UserRepository;
 public class Main {
     public static void main(String[] args) {
 
-        User[] user=new User[5];
-        user[0]=new User("omid","123");
-        user[1]=new User("mohammad","123");
-        user[2]=new User("javad","123");
-        user[3]=new User("nilofar","123");
-        user[4]=new User("fateme","123");
-        UserRepository userRepository=new UserRepository();
-        userRepository.saveAll(user);
+//        User[] user=new User[5];
+//        user[0]=new User("omid","123");
+//        user[1]=new User("mohammad","123");
+//        user[2]=new User("javad","123");
+//        user[3]=new User("nilofar","123");
+//        user[4]=new User("fateme","123");
+        UserRepository userRepository = new UserRepository();
+//        userRepository.saveAll(user);
 
         //userRepository.save(user);
         //System.out.println(user.getUserId());
         //userRepository.delete(1);
-        User showUser=userRepository.load(4);
-        System.out.println(showUser.toString());
-
-
+//        User showUser=userRepository.load(4);
+//        System.out.println(showUser.toString());
+        System.out.println(userRepository.load(3).toString());
+        User user2=userRepository.load(3);
+//        User user = new User(3, "reza", "123");
+//    userRepository.save(user);
+        user2.setPassword("234");
+        userRepository.update(user2);
+        System.out.println(userRepository.load(3).toString());
     }
 }
--- src/main/java/utility/Config.java
+++ src/main/java/utility/Config.java
@@ -3,6 +3,6 @@ package utility;
 public class Config {
     static final String URL_DB = "jdbc:postgresql://localhost:5432/jdbc";
     static final String USERNAME = "postgres";
-    static final String PASSWORD = "123";
+    static final String PASSWORD = "mra1298";
 
 }
